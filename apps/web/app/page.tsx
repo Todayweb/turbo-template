7a9d9e5d255ec7b740d58cb114915da70f4431b9
l0st0 <r.bilina@protonmail.com>
2024-10-17 22:16:16 +0200

    format files

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,13 @@
 import { prisma } from "@repo/db";
 
 export default async function Home() {
-	const users = await prisma.user.findMany();
-	console.log("NEXT_PUBLIC_TEST", process.env.NEXT_PUBLIC_TEST);
+  const users = await prisma.user.findMany();
+  console.log("NEXT_PUBLIC_TEST", process.env.NEXT_PUBLIC_TEST);
 
-	return (
-		<div>
-			<div>Users: {JSON.stringify(users)}</div>
-			<div>Env: {process.env.NEXT_PUBLIC_TEST}</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>Users: {JSON.stringify(users)}</div>
+      <div>Env: {process.env.NEXT_PUBLIC_TEST}</div>
+    </div>
+  );
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,19 +3,19 @@
 import type { ComponentProps, ReactNode } from "react";
 
 interface ButtonProps extends ComponentProps<"button"> {
-	children: ReactNode;
-	className?: string;
-	appName: string;
+  children: ReactNode;
+  className?: string;
+  appName: string;
 }
 
 export const Button = ({ children, className, appName, ...rest }: ButtonProps) => {
-	return (
-		<button
-			className={className}
-			onClick={() => alert(`Hello from your ${appName} app!`)}
-			{...rest}
-		>
-			{children}
-		</button>
-	);
+  return (
+    <button
+      className={className}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
