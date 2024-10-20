d1b3cbb3383f50abeac3992d2b36f10cdf1b5a94
l0st0 <r.bilina@protonmail.com>
2024-10-20 14:01:23 +0200

    setup tailwind in web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.variable} min-h-screen font-montserrat`}>{children}</body>
     </html>
   );
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,11 @@
+import { cn } from "@/utils/cn";
 import { prisma } from "@repo/db";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
-  console.log("NEXT_PUBLIC_TEST", process.env.NEXT_PUBLIC_TEST);
 
   return (
-    <div>
+    <div className={cn("bg-red-500")}>
       <div>Users: {JSON.stringify(users)}</div>
       <div>Env: {process.env.NEXT_PUBLIC_TEST}</div>
     </div>
