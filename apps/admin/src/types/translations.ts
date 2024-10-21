0e8a2b997bf17d396da15c5df5b3f189d95e4ba5
l0st0 <r.bilina@protonmail.com>
2024-10-21 21:22:15 +0200

    update translation types

--- apps/admin/src/types/translations.ts
+++ apps/admin/src/types/translations.ts
@@ -1,9 +1,6 @@
 import type { NestedKeyOf } from "next-intl";
-import type en from "../../messages/en.json";
-
-export type Messages = typeof en;
 
 export type ZodTranslations = unknown;
-export type ZodTranslationsSection<T extends keyof Messages> = (
-  key: NestedKeyOf<Pick<Messages, T>[T]>,
+export type ZodTranslationsSection<T extends keyof IntlMessages> = (
+  key: NestedKeyOf<Pick<IntlMessages, T>[T]>,
 ) => string;
