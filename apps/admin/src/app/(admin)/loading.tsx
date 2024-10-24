47ecc04c3ccbecab4b340760903c0cbd7342a7c1
l0st0 <r.bilina@protonmail.com>
2024-10-24 15:24:32 +0200

    update spinner

--- apps/admin/src/app/(admin)/loading.tsx
+++ apps/admin/src/app/(admin)/loading.tsx
@@ -2,8 +2,8 @@ import { Spin } from "antd";
 
 export default function Loading() {
   return (
-    <div className="w-full flex justify-center">
-      <Spin />
+    <div className="w-full h-full items-center flex justify-center">
+      <Spin size="large" />
     </div>
   );
 }
